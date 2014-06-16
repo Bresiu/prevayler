ecc1d0614749297f2875befc0ae74f87132b5d97
Bresiu <michal.brewczak@gmail.com>
2014-06-16 13:13:48 +0200

    added comments

--- src/Migration.java
+++ src/Migration.java
@@ -14,16 +14,19 @@ public class Migration {
     public static void go() throws MalformedURLException, ClassNotFoundException, NoSuchMethodException,
             InvocationTargetException, IllegalAccessException {
 
+        // 1. make List of URLs to pass to CustomClassLoader class
         URL url = new URL(PATH_TO_JAR);
-
         List<URL> urls = new ArrayList<URL>();
         urls.add(url);
 
+        // 2. Use CustomLoaderClass, to make sure, that loaded classes/methods are not from current project,
+        // but from the jar specified in URL, since Java class loaders (including URLClassLoader)
+        // first ask to load classes from their parent class loader.
         CustomClassLoader clsLoader = new CustomClassLoader(urls);
-                // new URL[]{new URL(PATH_TO_JAR)});
         Class cls = clsLoader.loadClass("RandomNumberGenerator");
-        Method method = cls.getMethod("foo");//, String[].class);
-        //String[] params = new String[2];
+        Method method = cls.getMethod("foo");
+
+        // 3. foo method just simply print out "bar" in console.
         method.invoke(null);
     }
     //*/
