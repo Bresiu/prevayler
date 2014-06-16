ccad2a80a6f32acc5eab4238b09a619dfed7dacf
Bresiu <michal.brewczak@gmail.com>
2014-06-16 17:01:37 +0200

    Added new field in Numbers

--- src/Numbers.java
+++ src/Numbers.java
@@ -5,7 +5,13 @@ import java.util.List;
 public class Numbers implements Serializable {
     private static final long serialVersionUID = 1l;
 
+    private static int intVariable = 1;
+
     private final ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+    public static int getIntVariable() {
+        return intVariable;
+    }
 
     public void addNumber(int number) {
         numbers.add(number);
